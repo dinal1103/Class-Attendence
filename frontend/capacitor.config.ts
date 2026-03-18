6a47b06e1d1e42c0d9fbe9987cc968ecbe1fcc7e
Ronak <ronakjaviya26@gmail.com>
2026-03-18 15:57:38 +0530

    update android configurations

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.college.attendance',
   appName: 'Smart Attendance',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https',
+    allowNavigation: ['ronak-pc.tailf0fdeb.ts.net', '*.ts.net']
+  }
 };
 
 export default config;
